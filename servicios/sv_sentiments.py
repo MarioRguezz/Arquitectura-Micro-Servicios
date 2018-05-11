18d6b575f20ef0fe437b514594d0456c25eee204
Antoniio <c-remix@hotmail.es>
2018-05-10 22:17:11 -0500

    Separación de sentiment con tweets

--- servicios/sv_sentiments.py
+++ servicios/sv_sentiments.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python
 # ----------------------------------------------------------------------------------------------------------------
-# Archivo: sv_sentiment.py
+# Archivo: sv_information.py
 # Tarea: 2 Arquitecturas Micro Servicios.
-# Autor(es): Equipo Alfeluma
-# Version: 1.1 Mayo 2018
+# Autor(es): Perla Velasco & Yonathan Mtz.
+# Version: 1.3 Octubre 2017
 # Descripción:
 #
 #   Este archivo define el rol de un servicio. Su función general es porporcionar en un objeto JSON
@@ -41,46 +41,24 @@ import conexion
 app = Flask(__name__)
 
 
-@app.route("/api/v1/tweets")
-def tweets():
+@app.route("/api/v1/sentiment")
+def sentiment():
     """
     Este método obtiene información acerca de una película o serie
     específica.
     :return: JSON con la información de la película o serie
     """
     # Se lee el parámetro 't' que contiene el título de la película o serie que se va a consultar
-    title = request.args.get("t")
-    api = twitter.Api(consumer_key='BUfe9SBJHUYCfBa8Fax14wmqn',
-                      consumer_secret='JYS7Cz1j5tfkZw7L44JcXzzgzt3cAkKn5LNl1jBIf4JnCCA03S',
-                      access_token_key='174333272-muKrJ9mlEfRwUwzoK5BKz1IrwwrqyIrnVj8LqZbO',
-                      access_token_secret='wPvxXEuBkI7KVJyLdJMvh0woD87gaElNuwDde7qlOslFo')
-    conexion.isSqliteExist()
-    search = api.GetSearch(title, count=50)
-    tweets = []
-    for tweet in search:
-        tweets.append({"id":tweet.id,"text":tweet.text})
-        conexion.storeTweet(tweet.id,tweet.text)
-    print json.dumps({"tweets":tweets})
-    
-    for tweet in conexion.selectTweets():
-        print tweet
-    
-    sentiments = {}
-
-    
-    for tweet in search:
-        r = requests.post("http://text-processing.com/api/sentiment/", data = {'text' : tweet.text})
-        response = json.loads(r.text)
-        if not response["label"] in sentiments:
-            sentiments[response["label"]] = 1
-        else:
-            sentiments[response["label"]] += 1
-    return json.dumps(sentiments), 200
+    tweet = request.args.get("tweet")
+    r = requests.post("http://text-processing.com/api/sentiment/", data = {'text' : tweet.text})
+    response = json.loads(r.text)
+    sentiment = {"sentiment":response["label"]}
+    return json.dumps(sentiment), 200
 
 
 if __name__ == '__main__':
     # Se define el puerto del sistema operativo que utilizará el servicio
-    port = int(os.environ.get('PORT', 8085))
+    port = int(os.environ.get('PORT', 8086))
     # Se habilita la opción de 'debug' para visualizar los errores
     app.debug = True
     # Se ejecuta el servicio definiendo el host '0.0.0.0' para que se pueda acceder desde cualquier IP
